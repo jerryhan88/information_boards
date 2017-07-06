9f944906e379dfe336ecfa9d3cb7e151fb158761
ckhan <chungkyun.han@gmail.com>
2017-07-06 17:06:36 +0800

    t

--- a_preprocessing/run.sh
+++ a_preprocessing/run.sh
@@ -4,9 +4,9 @@ for i in 090{1..9} 09{10..11}; do
     python -c "from a3_ap_tripLocation_prevTripEndTime import run; run('$i')" &
 done
 
-for i in 100{1..9} 10{11..12}; do
-    python -c "from a3_ap_tripLocation_prevTripEndTime import run; run('$i')" &
-done
+#for i in 100{1..9} 10{11..12}; do
+#    python -c "from a3_ap_tripLocation_prevTripEndTime import run; run('$i')" &
+#done
 
 
 #python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('09')" &
