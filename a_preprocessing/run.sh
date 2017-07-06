a5b5af0b721241c8284e41e8db1d3d1b910933d2
ckhan <chungkyun.han@gmail.com>
2017-07-06 16:15:10 +0800

    t

--- a_preprocessing/run.sh
+++ a_preprocessing/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #for i in 100{1..9} 10{11..12}; do
-for i in 090{1..9} 09{10..11}; do
+#for i in 090{1..9} 09{10..11}; do
 #    python -c "from a5_ap_qrTimeTerNumber import run; run('$i')" &
-    python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('$i')" &
+#done
 
-done
+
+python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('09')" &
+python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('10')" &
