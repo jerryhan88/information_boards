1678786c68e6d9f7ccc3dadbc0a184aacf9b5722
ckhan <chungkyun.han@gmail.com>
2017-07-14 11:12:06 +0800

    t

--- a_preprocessing/run.sh
+++ a_preprocessing/run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-#for i in 090{1..9} 09{10..11}; do
-#    python -c "from a3_ap_tripLocation_prevTripEndTime import run; run('$i')" &
+for i in 090{1..9} 09{10..11}; do
+    python -c "from a1_log_processing import run; run('$i')" &
+done
+
+#for i in 100{1..9} 10{11..12}; do
+#    python -c "from a1_log_processing import run; run('$i')" &
 #done
+
+
 
 #for i in 090{1..9} 09{10..11}; do
 #for i in 100{1..9} 10{11..12}; do
@@ -13,5 +19,5 @@
 #python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('09')" &
 #python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('10')" &
 
-python -c "from a6_ap_ecoProfit import run; run('09')" &
-python -c "from a6_ap_ecoProfit import run; run('10')" &
+#python -c "from a6_ap_ecoProfit import run; run('09')" &
+#python -c "from a6_ap_ecoProfit import run; run('10')" &
