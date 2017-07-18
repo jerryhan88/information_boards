8ce4d481ade4d9fc63bcda9741ea48a38619edd1
ckhan <chungkyun.han@gmail.com>
2017-07-18 23:41:58 +0800

    t

--- init_project.py
+++ init_project.py
@@ -36,7 +36,7 @@ for dn in [
             #
             'log', 'ap_dayLog',
             #
-            'trip', 'ap_dayTrip',
+            'trip', 'ap_dayTrip', 'ap_QTimeQNum'
             #
             #
             #
