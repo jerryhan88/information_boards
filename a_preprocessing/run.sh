a37ab924ffcc3c97c9cfba2460040d4b73c049ed
ckhan <chungkyun.han@gmail.com>
2017-07-18 13:23:09 +0800

    t

--- a_preprocessing/run.sh
+++ a_preprocessing/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-for i in 090{1..9} 09{10..11}; do
-    python -c "from a4_ap_dayTrip import run; run('$i')" &
-done
-
-#for i in 100{1..9} 10{11..12}; do
+#for i in 090{1..9} 09{10..11}; do
 #    python -c "from a4_ap_dayTrip import run; run('$i')" &
 #done
+
+for i in 100{1..9} 10{11..12}; do
+    python -c "from a4_ap_dayTrip import run; run('$i')" &
+done
 
 
 
