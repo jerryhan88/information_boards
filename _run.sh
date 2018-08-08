17e0043cb6e99c2639086dc878b86c59539b4684
ckhan <chungkyun.han@gmail.com>
2018-08-08 14:53:16 +0800

    t

---
+++ _run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+python -c "from a2_trip_processing import run; run('0911')" &
+
+
+
+#for i in 090{1..9} 09{10..11}; do
+#    python -c "from a4_ap_dayTrip import run; run('$i')" &
+#    python -c "from a5_ap_QidQNum import run; run('$i')" &
+#done
+
+#for i in 100{1..9} 10{11..12}; do
+#    python -c "from a4_ap_dayTrip import run; run('$i')" &
+#    python -c "from a5_ap_QidQNum import run; run('$i')" &
+#done
+
+
+
+#for i in 090{1..9} 09{10..11}; do
+#for i in 100{1..9} 10{11..12}; do
+#    python -c "from a5_ap_qrTimeTerNumber import run; run('$i')" &
+#done
+
+
+#python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('09')" &
+#python -c "from a5_ap_qrTimeTerNumber import filtering; filtering('10')" &
+
+#python -c "from a6_ap_ecoProfit import run; run('09')" &
+#python -c "from a6_ap_ecoProfit import run; run('10')" &
