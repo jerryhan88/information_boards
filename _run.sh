a557c6186ceb8507ea8d8511293acd419b696e0b
ckhan <chungkyun.han@gmail.com>
2018-08-10 15:04:14 +0800

    t

--- _run.sh
+++ _run.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash
 
-#python3 -c "from a1_log_processing import run; run('0911')" &
-#python3 -c "from a2_trip_processing import run; run('1001')" &
+#python3 -c "from util_cython import gen_cFile; gen_cFile('a1_log_processing')"
+#python3 -c "from util_cython import gen_cFile; gen_cFile('a2_trip_processing')"
 
-#python3 -c "from a3_ap_dayLog import run; run('0911')" &
-#python3 -c "from a3_ap_dayLog import run; run('1001')" &
+for i in 090{1..9} 0910; do
+    python3 -c "from a1_log_processing import run; run('$i')" &
+    python3 -c "from a2_trip_processing import run; run('$i')" &
+done
 
-#python3 -c "from a4_ap_dayTrip import run; run('0911')" &
-#python3 -c "from a4_ap_dayTrip import run; run('1001')" &
-
-
-
-python3 -c "from util_cython import gen_cFile; gen_cFile('a1_log_processing')"
-python3 -c "from util_cython import gen_cFile; gen_cFile('a2_trip_processing')"
-
-#for i in 090{1..9} 0910; do
-#    python3 -c "from a1_log_processing import run; run('$i')" &
-#    python3 -c "from a2_trip_processing import run; run('$i')" &
-#done
-
-#for i in 100{2..9} 10{11..12}; do
-#    python3 -c "from a1_log_processing import run; run('$i')" &
-#    python3 -c "from a2_trip_processing import run; run('$i')" &
-#done
+for i in 100{2..9} 10{11..12}; do
+    python3 -c "from a1_log_processing import run; run('$i')" &
+    python3 -c "from a2_trip_processing import run; run('$i')" &
+done
 
 
 
