c912e2568127d35aea41b6b78d5d5ad7a4ea197c
ckhan <chungkyun.han@gmail.com>
2018-08-14 14:02:30 +0800

    t

--- _run.sh
+++ _run.sh
@@ -3,9 +3,9 @@
 python3 -c "from util_cython import gen_cFile; gen_cFile('_log_extractor')"
 
 
-python3 -c "from _log_extractor import run; run('091101', hh='05', taxi_id=14964)" &
-python3 -c "from _log_extractor import run; run('091101', hh='06', taxi_id=14964)" &
-python3 -c "from _log_extractor import run; run('091101', hh='07', taxi_id=14964)" &
+python3 -c "from _log_extractor import run; run('091101', hh='05', taxi_id=6326)" &
+python3 -c "from _log_extractor import run; run('091101', hh='06', taxi_id=6326)" &
+python3 -c "from _log_extractor import run; run('091101', hh='07', taxi_id=6326)" &
 
 
 #python3 -c "from util_cython import gen_cFile; gen_cFile('a1_log_processing')"
