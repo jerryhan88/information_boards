03c6599d4a648beff5f34242ad0ba3abda654f70
ckhan <chungkyun.han@gmail.com>
2018-08-14 16:13:29 +0800

    t

--- _run.sh
+++ _run.sh
@@ -3,11 +3,14 @@
 #python3 -c "from util_cython import gen_cFile; gen_cFile('_log_extractor')"
 #python3 -c "from _log_extractor import run; run('091101', hh='05', taxi_id=6326)" &
 
-python3 -c "from util_cython import gen_cFile; gen_cFile('a1_log_processing')"
-python3 -c "from a1_log_processing import run; run('0911')" &
+#python3 -c "from util_cython import gen_cFile; gen_cFile('a1_log_processing')"
+#python3 -c "from a1_log_processing import run; run('0911')" &
 
 
-#python3 -c "from util_cython import gen_cFile; gen_cFile('a2_trip_processing')"
+python3 -c "from util_cython import gen_cFile; gen_cFile('a2_trip_processing')"
+python3 -c "from a2_trip_processing import run; run('0911')" &
+
+
 
 #for i in 090{1..9} 0910; do
 #    python3 -c "from a1_log_processing import run; run('$i')" &
--- a2_trip_processing.py
+++ a2_trip_processing.py
@@ -1,7 +1,7 @@
 import os.path as opath
 import csv
+from datetime import datetime
 from functools import reduce
-from traceback import format_exc
 #
 from util_geoFunctions import get_ap_polygons
 from util_logging import logging
@@ -14,130 +14,72 @@ MIN20 = 20 * 60
 
 def run(yymm):
     logging_fpath = opath.join(lf_dpath, 'a2_%s.txt' % yymm)
-    ofpath = opath.join(trip_dpath, 'trip-%s.csv' % yymm)
+    logging(logging_fpath, 'Start handling; %s' % yymm)
     #
-    logging(logging_fpath, 'handle the file; %s' % yymm)
-    if opath.exists(ofpath):
-        logging(logging_fpath, 'The file had already been processed; %s' % yymm)
-        return None
-    yy, mm = yymm[:2], yymm[-2:]
-    yyyy = '20%s' % yy
-    with open(ofpath, 'wt') as w_csvfile:
-        writer = csv.writer(w_csvfile, lineterminator='\n')
-        new_header = [
-            'year', 'month', 'day', 'dow', 'hour',
-            'taxi_id', 'driver_id', 'fare',
-            'start_latitude', 'start_longitude', 'start_time',
-            'end_latitude', 'end_longitude', 'end_time',
-            'start_loc', 'end_loc',
-            'time_first_free',
-        ]
-        writer.writerow(new_header)
+    yymm_dt = datetime.strptime(yymm, '%y%m')
+    yy, mm = yymm_dt.strftime('%y'), yymm_dt.strftime('%m')
+    yyyy = yymm_dt.strftime('%Y')
     #
     normal_fpath = reduce(opath.join, [TAXI_RAW_DATA_HOME,
                                        yyyy, mm, 'trips', 'trips-%s-normal.csv' % yymm])
     ext_fpath = reduce(opath.join, [TAXI_RAW_DATA_HOME,
                                        yyyy, mm, 'trips', 'trips-%s-normal-ext.csv' % yymm])
-    log_fpath = reduce(opath.join, [TAXI_RAW_DATA_HOME,
-                                    yyyy, mm, 'logs', 'logs-%s-normal.csv' % yymm])
-    handling_day = -1
-    try:
-        year, month = map(int, [yyyy, mm])
-        ap_polygons = get_ap_polygons()
-        vehicles = {}
-        with open(normal_fpath) as tripFileN:
-            tripReaderN = csv.DictReader(tripFileN)
-            with open(ext_fpath) as tripFileE:
-                tripReaderE = csv.DictReader(tripFileE)
-                with open(log_fpath) as logFile:
-                    logReader = csv.DictReader(logFile)
-                    for rowN in tripReaderN:
-                        rowE = next(tripReaderE)
-                        sDay, sDow, sHour = map(int, [rowN[cn] for cn in ['start-day', 'start-dow', 'start-hour']])
-                        if handling_day != sDay:
-                            logging(logging_fpath, 'handle day %d' % sDay)
-                            handling_day = sDay
-                        #
-                        tripTime = eval(rowN['start-time'])
-                        while True:
-                            rowL = next(logReader)
-                            logTime = eval(rowL['time'])
-                            vidL, state = map(int, [rowL[cn] for cn in ['vehicle-id', 'state']])
-                            if not vidL in vehicles:
-                                vehicles[vidL] = vehicle(vidL, logTime, state)
-                            else:
-                                vehicles[vidL].update(logTime, state)
-                            if tripTime <= logTime:
-                                break
-                        taxiID = int(rowN['vehicle-id'])
-                        if not taxiID in vehicles:
-                            continue
-                        driverID = int(rowE['driver-id'])
-                        startTime, endTime = map(eval, [rowN[cn] for cn in ['start-time', 'end-time']])
-                        timeFirstFree = vehicles[taxiID].firstFreeStateTime
-                        vehicles[taxiID].reset()
-                        #
-                        startLng, startLat = map(eval, [rowN[cn] for cn in ['start-long', 'start-lat']])
-                        endLng, endLat = map(eval, [rowN[cn] for cn in ['end-long', 'end-lat']])
-                        startLoc, endLoc = 'X', 'X'
-                        for ap_polygon in ap_polygons:
-                            if startLoc == 'X':
-                                if ap_polygon.is_including((startLng, startLat)):
-                                    startLoc = ap_polygon.name
-                            if endLoc == 'X':
-                                if ap_polygon.is_including((endLng, endLat)):
-                                    endLoc = ap_polygon.name
-                            if startLoc != 'X' and endLoc != 'X':
-                                break
-                        #
-                        with open(ofpath, 'a') as w_csvfile:
-                            writer = csv.writer(w_csvfile, lineterminator='\n')
-                            new_row = [year, month, sDay, sDow, sHour,
-                                       taxiID, driverID, rowN['fare'],
-                                       startLng, startLat, startTime,
-                                       endLng, endLat, endTime,
-                                       startLoc, endLoc,
-                                       timeFirstFree]
-                            writer.writerow(new_row)
-    except Exception as _:
-        logging(logging_fpath, format_exc())
-        raise
+    #
+    year, month = yymm_dt.year, yymm_dt.month
+    ap_polygons = get_ap_polygons()
+    handling_day, handling_hour = -1, -1
+    with open(normal_fpath) as tripFileN:
+        tripReaderN = csv.DictReader(tripFileN)
+        with open(ext_fpath) as tripFileE:
+            tripReaderE = csv.DictReader(tripFileE)
+            for rowN in tripReaderN:
+                rowE = next(tripReaderE)
+                #
+                startTime, endTime = map(eval, [rowN[cn] for cn in ['start-time', 'end-time']])
+                cur_dt = datetime.fromtimestamp(startTime)
+                if cur_dt.day != handling_day:
+                    handling_day = cur_dt.day
+                    logging(logging_fpath, 'handle day %d' % sDay)
+                    handling_hour = -1
+                if cur_dt.day == handling_day and cur_dt.hour != handling_hour:
+                    handling_hour = cur_dt.hour
+                    ofpath = opath.join(trip_dpath, 'trip-%s.csv' % cur_dt.strftime('%Y%m%d%H'))
+                    with open(ofpath, 'wt') as w_csvfile:
+                        writer = csv.writer(w_csvfile, lineterminator='\n')
+                        new_header = [
+                            'year', 'month', 'day', 'dow', 'hour',
+                            'taxi_id', 'driver_id', 'fare',
+                            'start_latitude', 'start_longitude', 'start_time',
+                            'end_latitude', 'end_longitude', 'end_time',
+                            'start_loc', 'end_loc',
+                        ]
+                        writer.writerow(new_header)
 
-
-class vehicle(object):
-    def __init__(self, vid, cl_time, cl_state):
-        self.vid = vid
-        self.pl_time, self.pl_state = cl_time, cl_state
-        #
-        if self.pl_state == FREE:
-            self.firstFreeStateTime = self.pl_time
-        else:
-            self.firstFreeStateTime = -1
-        #
-        if self.pl_state == ONCALL:
-            self.firstOnCallStateTime = self.pl_time
-        else:
-            self.firstOnCallStateTime = -1
-
-    def update(self, cl_time, cl_state):
-        if cl_state == FREE:
-            if self.firstFreeStateTime == -1:
-                self.firstFreeStateTime = cl_time
-            else:
-                if self.pl_state == FREE and MIN20 < cl_time - self.pl_time:
-                    self.firstFreeStateTime = cl_time
-        elif cl_state == ONCALL:
-            if self.firstOnCallStateTime == -1:
-                self.firstOnCallStateTime = cl_time
-        elif cl_state != POB:
-            self.reset()
-        else:
-            assert cl_state == POB
-        #
-        self.pl_time, self.pl_state = cl_time, cl_state
-
-    def reset(self):
-        self.firstFreeStateTime, self.firstOnCallStateTime = -1, -1
+                sDay, sDow, sHour = map(int, [rowN[cn] for cn in ['start-day', 'start-dow', 'start-hour']])
+                taxiID = int(rowN['vehicle-id'])
+                driverID = int(rowE['driver-id'])
+                #
+                startLng, startLat = map(eval, [rowN[cn] for cn in ['start-long', 'start-lat']])
+                endLng, endLat = map(eval, [rowN[cn] for cn in ['end-long', 'end-lat']])
+                startLoc, endLoc = 'X', 'X'
+                for ap_polygon in ap_polygons:
+                    if startLoc == 'X':
+                        if ap_polygon.is_including((startLng, startLat)):
+                            startLoc = ap_polygon.name
+                    if endLoc == 'X':
+                        if ap_polygon.is_including((endLng, endLat)):
+                            endLoc = ap_polygon.name
+                    if startLoc != 'X' and endLoc != 'X':
+                        break
+                #
+                with open(ofpath, 'a') as w_csvfile:
+                    writer = csv.writer(w_csvfile, lineterminator='\n')
+                    new_row = [year, month, sDay, sDow, sHour,
+                               taxiID, driverID, rowN['fare'],
+                               startLng, startLat, startTime,
+                               endLng, endLat, endTime,
+                               startLoc, endLoc]
+                    writer.writerow(new_row)
 
 
 if __name__ == '__main__':
