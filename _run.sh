68e93478e348a3ef3b36dce8f22d8c77ccfcccf6
ckhan <chungkyun.han@gmail.com>
2018-08-16 09:14:54 +0800

    t

--- _run.sh
+++ _run.sh
@@ -3,18 +3,18 @@
 #python3 -c "from util_cython import gen_cFile; gen_cFile('_log_extractor')"
 #python3 -c "from _log_extractor import run; run('091101', hh='05', taxi_id=6326)" &
 
-#python3 -c "from util_cython import gen_cFile; gen_cFile('a1_log_processing')"
-#python3 -c "from a1_log_processing import run; run('1002')" &
+python3 -c "from util_cython import gen_cFile; gen_cFile('a1_log_processing')"
+python3 -c "from a1_log_processing import run; run('0903')" &
 
 
 #python3 -c "from util_cython import gen_cFile; gen_cFile('a2_trip_processing')"
-#python3 -c "from a2_trip_processing import run; run('1002')" &
+#python3 -c "from a2_trip_processing import run; run('0903')" &
 
 
 #python3 -c "from util_cython import gen_cFile; gen_cFile('a3_dayTrip')"
 #python3 -c "from a3_dayTrip import run; run('1001')" &
 
-python3 -c "from util_cython import gen_cFile; gen_cFile('a4_apDayTrip')"
+#python3 -c "from util_cython import gen_cFile; gen_cFile('a4_apDayTrip')"
 #python3 -c "from a4_apDayTrip import run; run('-200911')" &
-python3 -c "from a4_apDayTrip import run; run('-201001')" &
+#python3 -c "from a4_apDayTrip import run; run('-201001')" &
 
